9edbbb4f2bfb67d6c3094a10e906efe17164bef6
Matt-tedesco98 <matthewtedesco3@gmail.com>
2025-03-31 01:20:53 -0400

    here we go

---
+++ src/features/Header/Header.jsx
@@ -0,0 +1,14 @@
+import { react } from "react";
+
+const Header = () => {
+    return (
+        <header>
+            <div className="logo">
+                <h1>logo placeholder</h1>
+                <p></p>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
