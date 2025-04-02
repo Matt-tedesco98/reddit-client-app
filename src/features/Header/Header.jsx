98ca8cb6dcb713ae0e345e475fc56259d1329bb6
matthewtedesco <matthewtedesco3@gmail.com>
2025-04-02 17:25:02 -0400

    added some more files

--- src/features/Header/Header.jsx
+++ src/features/Header/Header.jsx
@@ -1,12 +1,27 @@
-import { react } from "react";
+import react, { useState, useEffect } from "react";
+import { SiReddit } from "react-icons/si";
+import { HiOutlineSearchCircle } from "react-icons/hi";
+import './Header.css';
+
 
 const Header = () => {
     return (
         <header>
             <div className="logo">
-                <h1>logo placeholder</h1>
-                <p></p>
+                <SiReddit className="logo-icon"/>
+                <p>Meddit</p>
             </div>
+            <form className="search">
+                <input
+                    type="text"
+                    placeholder="Search"
+                    aria-label="Search"
+                />
+            <button type="submit" aria-label="Search">
+                <HiOutlineSearchCircle />
+            </button>
+
+            </form>
         </header>
     );
 }
